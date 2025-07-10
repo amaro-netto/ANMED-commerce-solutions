dd67ad2a0f547d028a57f0f8671d5adb953f765c
Amaro Netto <amarosilva002@gmail.com>
2025-07-10 19:12:11 +0000

    atualização de link do rodapé e correção de imagem do logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -70,8 +70,7 @@ const Footer = () => {
         </div>
 
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-<p>&copy; 2024 ANMED Produtos Médicos. Todos os direitos reservados. | Desenvolvido por <a href="[https://github.com/amaro-netto]">AMARO NETTO</a> </p>        </div>
-      </div>
+<p>&copy; 2024 ANMED Produtos Médicos. Todos os direitos reservados. | Desenvolvido por <a href="https://github.com/amaro-netto">AMARO NETTO</a> </p>      </div>
     </footer>
   );
 };
